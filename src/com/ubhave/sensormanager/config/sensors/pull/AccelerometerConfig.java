a44b2ceb7e97b6ba615229d35696c02db26cfefd
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-05-13 12:54:26 +0100

    Revert "Adding to manifest"
    This reverts commit a6457ff26290f2bd767afeb03280f34e0abd0876.

--- src/com/ubhave/sensormanager/config/sensors/pull/AccelerometerConfig.java
+++ src/com/ubhave/sensormanager/config/sensors/pull/AccelerometerConfig.java
@@ -13,8 +13,6 @@ public class AccelerometerConfig
 	public static final long DEFAULT_SAMPLING_WINDOW_SIZE_MILLIS = 8000;
 	public static final long DEFAULT_SLEEP_INTERVAL = 2 * 60 * 1000;
 	
-	/*
-	 * Classifier thresholds
-	 */
+	// classifier thresholds
 	public final static int ACCELEROMETER_MOVEMENT_THRESHOLD = 25;
 }
