c51f0018aa7680e83059178d261e06eb5c15722d
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2013-05-14 23:30:38 +0100

    refactor

--- src/com/ubhave/sensormanager/process/push/ScreenProcessor.java
+++ src/com/ubhave/sensormanager/process/push/ScreenProcessor.java
@@ -1,6 +1,7 @@
 package com.ubhave.sensormanager.process.push;
 
 import android.content.Context;
+import android.content.Intent;
 
 import com.ubhave.sensormanager.config.SensorConfig;
 import com.ubhave.sensormanager.data.pushsensor.ScreenData;
@@ -14,9 +15,20 @@ public class ScreenProcessor extends AbstractProcessor
 		super(c, rw, sp);
 	}
 	
-	public ScreenData process(long timestamp, SensorConfig config, int status)
+	public ScreenData process(long timestamp, SensorConfig config, Intent intent)
 	{
 		ScreenData data = new ScreenData(timestamp, config);
+		
+		int status = ScreenData.SCREEN_UNKNOWN;
+		if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
+		{
+			status = ScreenData.SCREEN_ON;
+		}
+		else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF))
+		{
+			status = ScreenData.SCREEN_OFF;
+		}
+		
 		data.setStatus(status);
 		return data;
 	}
--- src/com/ubhave/sensormanager/sensors/push/ScreenSensor.java
+++ src/com/ubhave/sensormanager/sensors/push/ScreenSensor.java
@@ -70,16 +70,7 @@ public class ScreenSensor extends AbstractPushSensor
 	protected void onBroadcastReceived(Context context, Intent intent)
 	{
 		ScreenProcessor processor = (ScreenProcessor) super.getProcessor();
-		int status = ScreenData.SCREEN_UNKNOWN;
-		if (intent.getAction().equals(Intent.ACTION_SCREEN_ON))
-		{
-			status = ScreenData.SCREEN_ON;
-		}
-		else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF))
-		{
-			status = ScreenData.SCREEN_OFF;
-		}
-		ScreenData screenData = processor.process(System.currentTimeMillis(), sensorConfig.clone(), status);
+		ScreenData screenData = processor.process(System.currentTimeMillis(), sensorConfig.clone(), intent);
 		onDataSensed(screenData);
 	}
 
