bb402d1e9b8481ce287a82e290f5eee2647069c0
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2014-04-08 18:11:43 +0100

    Adding app data obj

---
+++ src/com/ubhave/sensormanager/data/pullsensor/ApplicationData.java
@@ -0,0 +1,48 @@
+package com.ubhave.sensormanager.data.pullsensor;
+
+import java.util.HashMap;
+
+public class ApplicationData extends HashMap<String, String>
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5545990366264280102L;
+	private final static String APP_LABEL = "application_label";
+	private final static String BASE_ACTIVITY = "base_activity";
+	private final static String NUM_ACTIVITIES = "num_activities";
+	private final static String RUNNING_ACTIVITIES = "running_activities";
+	
+	public ApplicationData(String label, String base, int activities, int running)
+	{
+		put(APP_LABEL, label);
+		put(BASE_ACTIVITY, base);
+		put(NUM_ACTIVITIES, ""+activities);
+		put(RUNNING_ACTIVITIES, ""+running);
+	}
+	
+	public ApplicationData()
+	{
+		
+	}
+	
+	public String getLabel()
+	{
+		return get(APP_LABEL);
+	}
+	
+	public String getBaseActivityString()
+	{
+		return get(BASE_ACTIVITY);
+	}
+	
+	public int numberOfActivities()
+	{
+		return Integer.valueOf(get(NUM_ACTIVITIES));
+	}
+	
+	public int numberOfRunningActivities()
+	{
+		return Integer.valueOf(get(RUNNING_ACTIVITIES));
+	}
+}
