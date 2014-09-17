9abe347af2b7be9b9db19ff2d9b974313e22529a
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2014-09-17 13:32:43 +0100

    Missed accel/gyro processor commit

---
+++ src/com/ubhave/sensormanager/process/pull/AccelerometerProcessor.java
@@ -0,0 +1,29 @@
+package com.ubhave.sensormanager.process.pull;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+import com.ubhave.sensormanager.config.SensorConfig;
+import com.ubhave.sensormanager.data.pull.AbstractMotionData;
+import com.ubhave.sensormanager.data.pull.AccelerometerData;
+
+public class AccelerometerProcessor extends AbstractMotionProcessor
+{
+	public AccelerometerProcessor(final Context c, boolean rw, boolean sp)
+	{
+		super(c, rw, sp);
+	}
+
+	@Override
+	protected AbstractMotionData getInstance(long pullSenseStartTimestamp, SensorConfig sensorConfig)
+	{
+		return new AccelerometerData(pullSenseStartTimestamp, sensorConfig);
+	}
+
+	@Override
+	protected void processData(ArrayList<float[]> sensorReadings, ArrayList<Long> sensorReadingTimestamps, AbstractMotionData data)
+	{
+		// Future: feature extraction
+	}
+}
