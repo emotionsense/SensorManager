086ca3bcade7e8f3c4ef3909dabb9302fd1f3925
nlathia <neal.lathia@cl.cam.ac.uk>
2015-05-25 19:39:16 +0200

    Fix global config var visibility

--- src/com/ubhave/sensormanager/config/GlobalConfig.java
+++ src/com/ubhave/sensormanager/config/GlobalConfig.java
@@ -22,20 +22,15 @@ IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 package com.ubhave.sensormanager.config;
 
-
 public class GlobalConfig extends AbstractConfig
 {
-
-	// battery level at which sensing should be enabled/disabled
 	public final static String LOW_BATTERY_THRESHOLD = "LOW_BATTERY_THRESHOLD";
 	public final static String PRINT_LOG_D_MESSAGES = "PRINT_LOG_D_MESSAGES";
-
-	private final static boolean DEFAULT_PRINT_LOG_D_MESSAGES = true;
-	public static final int DEFAULT_LOW_BATTERY_THRESHOLD_LEVEL = 20;
-	
-	// acquire wake lock
 	public final static String ACQUIRE_WAKE_LOCK = "ACQUIRE_WAKE_LOCK";
 
+	private final static boolean DEFAULT_PRINT_LOG_D_MESSAGES = true;
+	private final static int DEFAULT_LOW_BATTERY_THRESHOLD_LEVEL = 20;
+	
 	private static GlobalConfig globalConfig;
 	private static final Object lock = new Object();
 
